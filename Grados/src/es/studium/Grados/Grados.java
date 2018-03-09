d92f4dbdeb6fe04b0c4ca004330cc3c40c3b3050
Xxracso40xX <35490277+Xxracso40xX@users.noreply.github.com>
2018-03-09 11:35:59 +0100

    Update Grados.java

--- Grados/src/es/studium/Grados/Grados.java
+++ Grados/src/es/studium/Grados/Grados.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+//He editado esta clase
 
 public class Grados extends Frame implements WindowListener, ActionListener
 {
