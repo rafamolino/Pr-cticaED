f8bc84d251d550abe4cbac2b4e88c7fc4a8b1fca
alvaromd94 <37107484+alvaromd94@users.noreply.github.com>
2018-03-09 11:47:20 +0100

    Update Grados.java

--- Grados/src/es/studium/Grados/Grados.java
+++ Grados/src/es/studium/Grados/Grados.java
@@ -25,7 +25,7 @@ public class Grados extends Frame implements WindowListener, ActionListener
 	
 	
 	
-	//He heditado esta clase (Álvaro M)
+	//He editado esta clase (Álvaro M)
 	public Grados()
 	{
 		setLayout( new GridLayout( 3,2 ) );
